3b842b006e5f4738219efbf7855d06a07b462d4b
sricharan2006 <charan.bukka@gmail.com>
2025-02-11 11:16:42 +0530

    add env

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
 const port = 3010;
-
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
@@ -13,3 +18,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+module.exports = config;
+console.log(config);
